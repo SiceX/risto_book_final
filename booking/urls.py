f2a8e3f14e74c8a49402b88b89b9cc4b0912b021
Sice <30990328+SiceX@users.noreply.github.com>
2021-07-20 21:40:11 +0200

    ListView per i tavoli. Potrebbe diventare la pagina con la lista dei prenotabili magari

--- booking/urls.py
+++ booking/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from booking.views import TavoloDetail
-
+from booking.views import TavoloDetail, TavoloList
 
 app_name = 'booking'
 
 
 urlpatterns = [
-	path('tavolo/<int:pk>/detail', TavoloDetail.as_view(), name='tavolo-detail')
+	path('tavoli/<int:pk>/detail', TavoloDetail.as_view(), name='tavolo-detail'),
+	path('tavoli/list', TavoloList.as_view(), name='tavolo-list')
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,6 @@
 import logging
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, ListView
 from booking.models import Tavolo
 
 _logger = logging.getLogger(__name__)
@@ -9,3 +9,8 @@ _logger = logging.getLogger(__name__)
 class TavoloDetail(DetailView):
 	model = Tavolo
 	template_name = 'booking/tavolo/detail.html'
+
+
+class TavoloList(ListView):
+	model = Tavolo
+	template_name = 'booking/tavolo/list.html'
