f2468b0ecaad87957c203d45b14c072b36cbf0c3
Sice <30990328+SiceX@users.noreply.github.com>
2021-07-22 12:01:09 +0200

    Aggiunto datepicker per selezionare il giorno da visualizzare nella dashboard

--- booking/views.py
+++ booking/views.py
@@ -1,11 +1,14 @@
 import datetime
 import logging
+from functools import partial
 
 from django import forms
 from django.core.exceptions import ObjectDoesNotExist
+from django.forms import DateInput
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, DetailView, ListView, CreateView
+from django.views.generic.edit import FormMixin
 from extra_views import ModelFormSetView
 
 from booking.models import Tavolo, Prenotazione
@@ -30,13 +33,42 @@ class TavoloList(ListView):
 	template_name = 'booking/tavolo/list.html'
 
 
-class DashboardPrenotazioni(ListView):
+class DateNavForm(forms.Form):
+	selected_date = forms.DateTimeField(widget=DateInput())
+
+
+class DashboardPrenotazioni(ListView, FormMixin):
 	model = Tavolo
 	template_name = 'booking/prenotazione/dashboard_prenotazioni.html'
+	form_class = DateNavForm
+
 	year = None
 	month = None
 	day = None
 	hour = None
+
+	def get_success_url(self):
+		return reverse_lazy('booking:dashboard-prenotazioni', kwargs={
+			'year': self.year,
+			'month': self.month,
+			'day': self.day,
+			'hour': self.hour
+		})
+
+	def post(self, request, *args, **kwargs):
+		form = self.get_form()
+		if form.is_valid():
+			return self.form_valid(form)
+		else:
+			return self.form_invalid(form)
+
+	def form_valid(self, form):
+		selected_date = form.cleaned_data["selected_date"]
+		self.year = selected_date.year
+		self.month = selected_date.month
+		self.day = selected_date.day
+		self.hour = 12
+		return super().form_valid(form)
 
 	def get_context_data(self, **kwargs):
 		ctx = super(DashboardPrenotazioni, self).get_context_data(**kwargs)
@@ -55,9 +87,3 @@ class DashboardPrenotazioni(ListView):
 		ctx['tavoli_prenotati'] = tavoli_prenotati
 
 		return ctx
-
-
-# class TavoloAbilitateList(ModelFormSetView):
-# 	model = Tavolo
-# 	template_name = 'booking/tavolo/updateList.html'
-# 	#form_class = forms.EditListForm
