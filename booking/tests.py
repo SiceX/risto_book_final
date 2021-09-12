90cf492e8434f263f5f917b5125c1adcc9b4f966
Sice <30990328+SiceX@users.noreply.github.com>
2021-09-12 22:23:51 +0200

    Abbozzo unittest

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Prenotazione
+
+
+class PrenotazioneTests(TestCase):
+	def test_qualcosa(self):
+		self.assertTrue(True)
+
+
+class DashboardViewTests(TestCase):
+	def test_qualcosaltro(self):
+		response = self.client.get(reverse('booking:dashboard-prenotazioni',
+										   kwargs={'year': '2021',
+												   'month': '09',
+										   			'day': '12'}))
+		self.assertEqual(response.status_code, 200)
