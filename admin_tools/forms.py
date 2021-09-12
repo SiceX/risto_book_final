fab906d46d5024c0a5160976f43a82de946eaee8
Sice <30990328+SiceX@users.noreply.github.com>
2021-09-12 04:16:00 +0200

    WIP form layout per la tabella

--- admin_tools/forms.py
+++ admin_tools/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from crispy_forms.layout import Submit, Layout, Field, Button, ButtonHolder, HTML
+from crispy_forms.layout import Submit, Layout, Field, Button, ButtonHolder, HTML, Fieldset
 from crispy_forms.helper import FormHelper
 
 from booking.models import Tavolo
@@ -7,28 +7,30 @@ from booking.models import Tavolo
 
 class GestisciTavoliForm(forms.ModelForm):
 
-	helper = FormHelper()
-	helper.form_id = 'manage_tavoli_crispy_form'
-	helper.form_method = 'POST'
-	helper.add_input(Submit('submit', 'Conferma'))
-	helper.inputs[0].field_classes = 'btn btn-success'
-	# helper.layout = Layout(
-	# 	Field('tavolo', readonly="readonly"),
-	# 	Field('data_ora', readonly="readonly"),
-	# 	Field('queue_place', readonly="readonly")
-	# )
-
-	# def __init__(self, *args, **kwargs):
-	# 	super(PrenotazioneForm, self).__init__(*args, **kwargs)
-	# 	self.fields['tavolo'].disabled = True
-	# 	self.fields['tavolo'].required = False
-	# 	self.fields['queue_place'].required = False
-	#
-	# 	if 'tavolo' not in self.initial:
-	# 		self.fields['tavolo'].widget = HiddenInput()
-	# 	if 'queue_place' not in self.initial:
-	# 		self.fields['queue_place'].widget = HiddenInput()
-
 	class Meta:
 		model = Tavolo
 		fields = ['nome', 'abilitato']
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+		tavolo = self.initial['nome']
+
+		layout = Layout(
+			HTML("""<tr>"""),
+			Fieldset(
+				HTML("""<td>"""),
+				'nome',
+				HTML("""</td>"""),
+				HTML("""<td>"""),
+				'abilitato',
+				HTML("""</td>"""),
+				HTML("""<td> <a href="{% url 'admin_tools:tavolo-detail' """ + tavolo + """ %}">Dettaglio</a> </td>"""),
+			),
+			HTML("""</tr>""")
+		)
+
+		self.helper = FormHelper()
+		self.helper.form_id = 'manage_tavoli_crispy_form'
+		self.helper.form_method = 'POST'
+		self.helper.layout = layout
