046471666056ce07533e877352d58e82a8caab54
Sice <30990328+SiceX@users.noreply.github.com>
2021-09-12 05:21:12 +0200

    Bugfix, pagina gestione tavoli quasi ultimata

--- admin_tools/forms.py
+++ admin_tools/forms.py
@@ -13,26 +13,8 @@ class GestisciTavoliForm(forms.ModelForm):
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-
-		# layout = None
-		# if 'nome' in self.initial:
-		# 	tavolo = self.initial['nome']
-		#
-		# 	layout = Layout(
-		# 		HTML("""<tr>"""),
-		# 		Fieldset(
-		# 			HTML("""<td>"""),
-		# 			'nome',
-		# 			HTML("""</td>"""),
-		# 			HTML("""<td>"""),
-		# 			'abilitato',
-		# 			HTML("""</td>"""),
-		# 			HTML("""<td> <a href="{% url 'admin_tools:tavolo-detail' """ + tavolo + """ %}">Dettaglio</a> </td>"""),
-		# 		),
-		# 		HTML("""</tr>""")
-		# 	)
+		self.fields['nome'].required = True
 
 		self.helper = FormHelper()
 		self.helper.form_id = 'manage_tavoli_crispy_form'
 		self.helper.form_method = 'POST'
-		# self.helper.layout = layout
