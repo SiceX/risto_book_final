7a5f7c8b7aba30e5d5b557ed42f28f13d1b4b471
Sice <30990328+SiceX@users.noreply.github.com>
2021-09-14 21:19:35 +0200

    Uso clean() invece di save() per fare i controlli adesso.

--- booking/models.py
+++ booking/models.py
@@ -40,10 +40,15 @@ class Prenotazione(models.Model):
 						   ['data_ora', 'tavolo']]
 		verbose_name_plural = 'Prenotazioni'
 
-	def save(self, *args, **kwargs):
+	def clean(self):
+		super().clean()
 		if self.data_ora < (timezone.now().date() + datetime.timedelta(days=1)):
-			raise ValidationError("Non è possibile creare nuove prenotazioni per oggi o nel passato")
-		super(Prenotazione, self).save(*args, **kwargs)
+			raise ValidationError("non è possibile creare nuove prenotazioni per oggi o nel passato")
+
+		if self.tavolo is None and self.queue_place is None:
+			raise ValidationError("tavolo e queue_place sono entrambi nulli")
+		if self.tavolo is not None and self.queue_place is not None:
+			raise ValidationError("tavolo e queue_place sono entrambi valorizzati")
 
 	def __str__(self):
 		return f'{self.tavolo} - {self.data_ora} - {self.pk}'
