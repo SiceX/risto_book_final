174d787b8f85e374ab2bdd3bc71f665b8c46aa0f
Sice <30990328+SiceX@users.noreply.github.com>
2021-09-14 20:40:54 +0200

    Visualizzazione data scelta

--- booking/templatetags/common_tags.py
+++ booking/templatetags/common_tags.py
@@ -8,3 +8,10 @@ register = template.Library()
 def calc_date_from_now(format, days):
     tomorrow_date = datetime.date.today() + datetime.timedelta(days=days)
     return tomorrow_date.strftime(format)
+
+
+@register.simple_tag
+def get_date(year, month, day):
+    return datetime.datetime.strptime(f"{year}-{month}-{day}", "%Y-%m-%d")
+    # return date.weekday()
+
