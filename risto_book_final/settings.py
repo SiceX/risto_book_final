64897f674b5da0c5ed4d19272c5f42f64ea1ee40
Sice <30990328+SiceX@users.noreply.github.com>
2021-09-14 17:47:07 +0200

    Ritenuti giusti i settings MEDIA_ROOT e MEDIA_URL dal fallito tentativo di implementare una propic.

--- risto_book_final/settings.py
+++ risto_book_final/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
+MEDIA_ROOT = [BASE_DIR, '/media']
+MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
