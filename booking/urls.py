2de91e85758ed0cbf8164b9ff3832b05b4df9ee4
Sice <30990328+SiceX@users.noreply.github.com>
2021-09-14 19:41:41 +0200

    Controlli per evitare di poter visualizzare la dashboard prenotazioni in date minori di domani

--- booking/urls.py
+++ booking/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from booking.views import DashboardPrenotazioni, PrenotazioneCreate, \
-	PrenotazioneList, PrenotazioneDelete, FindFirstFreeDayRedirectView, FindFirstFreeDayForTableRedirectView
+	PrenotazioneList, PrenotazioneDelete, FindFirstFreeDayRedirectView, FindFirstFreeDayForTableRedirectView, \
+	dashboardPrenotazioniController
 
 app_name = 'booking'
 
 urlpatterns = [
 	path('prenotazioni/list', PrenotazioneList.as_view(), name='prenotazione-list'),
 	path('prenotazioni/<str:pk>/delete', PrenotazioneDelete.as_view(), name='prenotazione-delete'),
-	path('prenotazioni/dashboard/<int:year>/<int:month>/<int:day>', DashboardPrenotazioni.as_view(),
+	path('prenotazioni/dashboard/<int:year>/<int:month>/<int:day>', dashboardPrenotazioniController,
 		name='dashboard-prenotazioni'),
 	path('prenotazioni/dashboard/find-free', FindFirstFreeDayRedirectView.as_view(),
 		name='dashboard-prenotazioni-find'),
--- booking/views.py
+++ booking/views.py
@@ -11,7 +11,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail
 from django.db.models import F
 from django.forms import DateInput
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, DetailView, ListView, CreateView, RedirectView
@@ -113,6 +113,17 @@ class PrenotazioneCreate(LoginRequiredMixin, CreateView):
 		self.object.utente = self.request.user
 		self.object.save()
 		return HttpResponseRedirect(self.get_success_url())
+
+
+def dashboardPrenotazioniController(request, year, month, day):
+	coso = None
+	kwargs = {'year': year, 'month': month, 'day': day}
+	date = datetime(year, month, day)
+	if date.date() < (timezone.now().date() + timedelta(days=1)):
+		tomorrow = datetime.now().date() + timedelta(days=1)
+		kwargs = {'year': tomorrow.year, 'month': tomorrow.month, 'day': tomorrow.day}
+		return DashboardPrenotazioni.as_view()(request, **kwargs)
+	return DashboardPrenotazioni.as_view()(request, **kwargs)
 
 
 class DashboardPrenotazioni(ListView, FormMixin):
