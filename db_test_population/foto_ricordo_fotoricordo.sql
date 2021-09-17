b7a6345a646d64419219345964d86e40b73e7a32
Sice <30990328+SiceX@users.noreply.github.com>
2021-09-17 13:55:16 +0200

    rimosso foto del drago

--- db_test_population/foto_ricordo_fotoricordo.sql
+++ db_test_population/foto_ricordo_fotoricordo.sql
@@ -1,4 +1,3 @@
-INSERT INTO foto_ricordo_fotoricordo (id, foto_ricordo, data) VALUES (5, 'foto_ricordo/20210916-212649-ThumbsUp.jpg', '2021-09-16 21:26:49.096629');
 INSERT INTO foto_ricordo_fotoricordo (id, foto_ricordo, data) VALUES (6, 'foto_ricordo/20210916-212658-missile.jpeg', '2021-09-16 21:26:58.344658');
 INSERT INTO foto_ricordo_fotoricordo (id, foto_ricordo, data) VALUES (7, 'foto_ricordo/20210916-212824-gratito.jpeg', '2021-09-16 21:28:24.389662');
 INSERT INTO foto_ricordo_fotoricordo (id, foto_ricordo, data) VALUES (8, 'foto_ricordo/20210916-212856-bebop1.jpg', '2021-09-16 21:28:56.144067');
